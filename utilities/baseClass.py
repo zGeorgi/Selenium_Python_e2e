f353ad6b48c712f4a1915e9d7396092cdf17b8dd
zGeorgi <gzayakov1@gmail.com>
2020-08-19 22:53:58 +0100

    fix the duplicates in logfile Second version

--- utilities/baseClass.py
+++ utilities/baseClass.py
@@ -17,11 +17,9 @@ class BaseClass:
         logging.getLogger(test_name).handlers.clear()
         log = logging.getLogger(test_name)
 
-       # if not len(log.handlers):
         file_h = logging.FileHandler("/home/georgi/PycharmProjects/E_Shop/utilities/logfile.log")
         formatter = logging.Formatter("%(asctime)s => %(levelname)s => %(name)s => %(message)s")
         file_h.setFormatter(formatter)
-
         log.addHandler(file_h)
         log.setLevel(logging.DEBUG)
         return log
